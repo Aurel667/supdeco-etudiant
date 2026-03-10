5741579ee88ecb00bd3666f445cc08bd6e9e9084
Aurel KOLANI <aureliuskolani@gmail.com>
2026-03-10 20:12:45 +0000

    update: app loading

--- src/utils/guestProtectedRoute.tsx
+++ src/utils/guestProtectedRoute.tsx
@@ -1,8 +1,14 @@
 import {Outlet, Navigate} from "react-router-dom";
 import useAuthStore from "../stores/auth.store";
+import Loader from "../components/loaders/loader";
 
 export default function GuestProtectedRoutes(){
-    const {user} = useAuthStore()
+    const {user, isLoading} = useAuthStore()
+    if(isLoading){
+        return (<div className="h-[100vh] w-full flex items-center justify-center">
+            <Loader className="w-12 h-12" />
+        </div>)
+    }
     if(user){
         return <Navigate to={"/"} />
     }
